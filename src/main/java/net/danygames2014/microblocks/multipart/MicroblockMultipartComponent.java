a9df80bce7f6ec79f500782924c63a2b652f1465
Ralf2oo2 <ralf2oo2@hotmail.com>
2026-01-27 00:43:43 +0000

    remove middle corners

--- src/main/java/net/danygames2014/microblocks/multipart/MicroblockMultipartComponent.java
+++ src/main/java/net/danygames2014/microblocks/multipart/MicroblockMultipartComponent.java
@@ -3,5 +3,6 @@ package net.danygames2014.microblocks.multipart;
 import net.danygames2014.nyalib.multipart.MultipartComponent;
 
 public abstract class MicroblockMultipartComponent extends MultipartComponent {
+    public PlacementSlot slot;
 
 }
