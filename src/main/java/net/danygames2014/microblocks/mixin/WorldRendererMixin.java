85253482a256e60c44ae27f1dfaf69db2c8fdffa
Ralf2oo2 <ralf2oo2@hotmail.com>
2026-02-04 12:55:58 +0000

    better preview rendering

--- src/main/java/net/danygames2014/microblocks/mixin/WorldRendererMixin.java
+++ src/main/java/net/danygames2014/microblocks/mixin/WorldRendererMixin.java
@@ -3,10 +3,13 @@ package net.danygames2014.microblocks.mixin;
 import net.danygames2014.microblocks.client.render.MicroblockRenderer;
 import net.danygames2014.microblocks.item.base.MicroblockItem;
 import net.danygames2014.microblocks.multipart.PlacementSlot;
+import net.danygames2014.microblocks.multipart.model.MicroblockModel;
 import net.danygames2014.microblocks.multipart.placement.PlacementHelper;
 import net.danygames2014.microblocks.util.DirectionUtil;
 import net.danygames2014.microblocks.util.MathHelper;
+import net.danygames2014.nyalib.multipart.MultipartState;
 import net.danygames2014.nyalib.util.PlayerUtil;
+import net.minecraft.block.Block;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -32,29 +35,47 @@ public class WorldRendererMixin {
     void renderGrid(PlayerEntity player, HitResult hitResult, int i, ItemStack handStack, float tickDelta, CallbackInfo ci){
         if(i == 0 && hitResult.type == HitResultType.BLOCK && player.getHand() != null && player.getHand().getItem() instanceof MicroblockItem microblockItem){
             microblockItem.renderGrid(player, hitResult.blockX, hitResult.blockY, hitResult.blockZ, new Vec3d(hitResult.pos.x, hitResult.pos.y, hitResult.pos.z), Direction.byId(hitResult.side), tickDelta);
-            PlacementHelper placementHelper = microblockItem.getPlacementHelper();
-            BlockPos placementPos = MathHelper.getPlacementPos(hitResult.blockX, hitResult.blockY, hitResult.blockZ, Direction.byId(hitResult.side));
-            PlacementSlot placementSlot = placementHelper.getSlot(placementPos.x, placementPos.y, placementPos.z, Direction.byId(hitResult.side), new Vec3d(hitResult.pos.x, hitResult.pos.y, hitResult.pos.z), 1/4D);
-            if (player.isSneaking()) {
-                placementSlot = placementHelper.getOppositeSlot(placementSlot, Direction.byId(hitResult.side));
+            int size = microblockItem.getSize();
+            Direction direction = Direction.byId(hitResult.side);
+            Vec3d stapiVec = new Vec3d(hitResult.pos.x, hitResult.pos.y, hitResult.pos.z);
+
+            MultipartState state = world.getMultipartState(hitResult.blockX, hitResult.blockY, hitResult.blockZ);
+            Vec3d relativeHitVec = stapiVec.add(-hitResult.blockX, -hitResult.blockY, -hitResult.blockZ);
+
+            if(state != null && MathHelper.getHitDepth(relativeHitVec, direction) < 1){
+                if(tryRenderPreview(world, hitResult.blockX, hitResult.blockY, hitResult.blockZ, direction, stapiVec, microblockItem.getSize(), microblockItem.getMicroblockModel(), microblockItem.block, handStack.getDamage(), microblockItem.getPlacementHelper(), player, tickDelta)){
+                    ci.cancel();
+                    return;
+                }
             }
 
-            if(placementHelper.canPlace(world, placementPos.x, placementPos.y, placementPos.z, placementSlot, microblockItem.getSize(), microblockItem.getMicroblockModel())){
-                MicroblockRenderer renderer = MicroblockRenderer.INSTANCE;
-                GL11.glPushMatrix();
-                GL11.glEnable(GL11.GL_BLEND);
-                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            BlockPos pPos = MathHelper.getPlacementPos(hitResult.blockX, hitResult.blockY, hitResult.blockZ, direction);
 
-                Vec3d playerPos = PlayerUtil.getRenderPosition(player, tickDelta);
-                GL11.glTranslated(placementPos.x - playerPos.x, placementPos.y - playerPos.y, placementPos.z - playerPos.z);
-
-                renderer.renderMicroblockPreview(microblockItem.getMicroblockModel(), placementSlot, microblockItem.block, handStack.getDamage(), microblockItem.getSize(), 0, 0, 0);
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopMatrix();
-            }
-
+            tryRenderPreview(world, pPos.getX(), pPos.getY(), pPos.getZ(), direction, stapiVec, microblockItem.getSize(), microblockItem.getMicroblockModel(), microblockItem.block, handStack.getDamage(), microblockItem.getPlacementHelper(), player, tickDelta);
 
             ci.cancel();
         }
+    }
+    boolean tryRenderPreview(World world, int x, int y, int z, Direction dir, Vec3d vec, int size, MicroblockModel microblockModel, Block block, int meta, PlacementHelper placementHelper, PlayerEntity player, float tickDelta){
+        PlacementSlot placementSlot = placementHelper.getSlot(x, y, z, dir, vec, 1/4D);
+        if (player.isSneaking()) {
+            placementSlot = placementHelper.getOppositeSlot(placementSlot, dir);
+        }
+
+        if(placementHelper.canPlace(world, x, y, z, placementSlot, size, microblockModel)){
+            MicroblockRenderer renderer = MicroblockRenderer.INSTANCE;
+            GL11.glPushMatrix();
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+            Vec3d playerPos = PlayerUtil.getRenderPosition(player, tickDelta);
+            GL11.glTranslated(x - playerPos.x, y - playerPos.y, z - playerPos.z);
+
+            renderer.renderMicroblockPreview(microblockModel, placementSlot, block, meta, size, 0, 0, 0);
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glPopMatrix();
+            return true;
+        }
+        return false;
     }
 }
