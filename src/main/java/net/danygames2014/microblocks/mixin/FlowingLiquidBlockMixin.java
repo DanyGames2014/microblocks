44b9f30fc8e0dbde09ba7f2be41b804d8d0cd851
Daniel Fildán <fildand@centrum.cz>
2026-02-10 01:21:04 +0100

    Get fluid collision with microblocks working

--- src/main/java/net/danygames2014/microblocks/mixin/FlowingLiquidBlockMixin.java
+++ src/main/java/net/danygames2014/microblocks/mixin/FlowingLiquidBlockMixin.java
@@ -1,7 +1,6 @@
 package net.danygames2014.microblocks.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.danygames2014.nyalib.multipart.MultipartState;
 import net.minecraft.block.FlowingLiquidBlock;
@@ -9,57 +8,121 @@ import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.selectors.ITargetSelectorByName;
 
+@SuppressWarnings("RedundantIfStatement")
 @Mixin(FlowingLiquidBlock.class)
 public class FlowingLiquidBlockMixin {
-    @WrapOperation(method = "getSpread", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;isLiquidBreaking(Lnet/minecraft/world/World;III)Z", ordinal = 0))
-    public boolean modifySpreadForMicroblocks(FlowingLiquidBlock block, World world, int x, int y, int z, Operation<Boolean> original, @Local(ordinal = 3) int var5) {
-        MultipartState state = world.getMultipartState(x, y, z);
+    @WrapWithCondition(method = "onTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;spreadTo(Lnet/minecraft/world/World;IIII)V", ordinal = 0))
+    public boolean checkSpreadNegativeX(FlowingLiquidBlock block, World world, int targetX, int targetY, int targetZ, int depth, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+        MultipartState state = world.getMultipartState(x,y,z);
+        MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
         
-        switch (var5) {
-            // X-
-            case 0 -> {
-                if (state != null && state.isBoxOccupied(Box.create(x + 1, y, z, x + 1 + 0.0625D, y + 1, z + 1))) {
-                    return true;
-                }
-                
-                MultipartState neighborState = world.getMultipartState(x, y, z);
-                if (neighborState != null && neighborState.isBoxOccupied(Box.create(x + 0.9375D, y, z, x + 1, y + 1, z + 1))) {
-                    return true;
-                }
-            }
-            
-            // X+
-            case 1 -> {
-                if (state != null && state.isBoxOccupied(Box.create(x, y, z, x + 0.9375D, y + 1, z + 1))) {
-                    return true;
-                }
-
-                MultipartState neighborState = world.getMultipartState(x - 1, y, z);
-                if (neighborState != null && neighborState.isBoxOccupied(Box.create(x + 0.0625D, y, z, x + 1, y + 1, z + 1))) {
-                    return true;
-                }
-            }
-            
-            // Z-
-            case 2 -> {
-                
-            }
-            
-            // Z+
-            case 3 -> {
-                
-            }
+        if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z, x + 0.0625D, y + 1, z + 1))) {
+            return false;
         }
         
-        return original.call(block, world, x, y, z);
+        if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX + 0.9375D, targetY, targetZ, targetX + 1, targetY + 1, targetZ + 1))) {
+            return false;
+        }
+        
+        return true;
     }
 
-    @WrapOperation(method = "getSpread", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;isLiquidBreaking(Lnet/minecraft/world/World;III)Z", ordinal = 1))
-    public boolean modifySpreadForMicroblocks(FlowingLiquidBlock block, World world, int x, int y, int z, Operation<Boolean> original) {
-        // Y-
+    @WrapWithCondition(method = "onTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;spreadTo(Lnet/minecraft/world/World;IIII)V", ordinal = 1))
+    public boolean checkSpreadPositiveX(FlowingLiquidBlock block, World world, int targetX, int targetY, int targetZ, int depth, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+        MultipartState state = world.getMultipartState(x,y,z);
+        MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
 
-        return original.call(block, world, x, y, z);
+        if (state != null && state.isBoxFullyCoevered(Box.create(x + 0.9375D, y, z, x + 1, y + 1, z + 1))) {
+            return false;
+        }
+
+        if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ, targetX + 0.0625D, targetY + 1, targetZ + 1))) {
+            return false;
+        }
+
+        return true;
     }
+
+    @WrapWithCondition(method = "onTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;spreadTo(Lnet/minecraft/world/World;IIII)V", ordinal = 2))
+    public boolean checkSpreadNegativeZ(FlowingLiquidBlock block, World world, int targetX, int targetY, int targetZ, int depth, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+        MultipartState state = world.getMultipartState(x,y,z);
+        MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+
+        if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z, x + 1, y + 1, z + 0.0625D))) {
+            return false;
+        }
+
+        if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ + 0.9375D, targetX + 1, targetY + 1, targetZ + 1))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @WrapWithCondition(method = "onTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;spreadTo(Lnet/minecraft/world/World;IIII)V", ordinal = 3))
+    public boolean checkSpreadPositiveZ(FlowingLiquidBlock block, World world, int targetX, int targetY, int targetZ, int depth, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+        MultipartState state = world.getMultipartState(x,y,z);
+        MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+
+        if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z  + 0.9375D, x + 1, y + 1, z + 1))) {
+            return false;
+        }
+
+        if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ, targetX + 1, targetY + 1, targetZ + 0.0625D))) {
+            return false;
+        }
+
+        return true;
+    }
+
+//    @WrapOperation(method = "getSpread", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;isLiquidBreaking(Lnet/minecraft/world/World;III)Z", ordinal = 0))
+//    public boolean modifySpreadForMicroblocks(FlowingLiquidBlock block, World world, int x, int y, int z, Operation<Boolean> original, @Local(ordinal = 3) int var5) {
+//        MultipartState state = world.getMultipartState(x, y, z);
+//
+//        switch (var5) {
+//            // X-
+//            case 0 -> {
+//                if (state != null && state.isBoxOccupied(Box.create(x + 1, y, z, x + 1 + 0.0625D, y + 1, z + 1))) {
+//                    return true;
+//                }
+//
+//                MultipartState neighborState = world.getMultipartState(x, y, z);
+//                if (neighborState != null && neighborState.isBoxOccupied(Box.create(x + 0.9375D, y, z, x + 1, y + 1, z + 1))) {
+//                    return true;
+//                }
+//            }
+//
+//            // X+
+//            case 1 -> {
+//                if (state != null && state.isBoxOccupied(Box.create(x, y, z, x + 0.9375D, y + 1, z + 1))) {
+//                    return true;
+//                }
+//
+//                MultipartState neighborState = world.getMultipartState(x - 1, y, z);
+//                if (neighborState != null && neighborState.isBoxOccupied(Box.create(x + 0.0625D, y, z, x + 1, y + 1, z + 1))) {
+//                    return true;
+//                }
+//            }
+//
+//            // Z-
+//            case 2 -> {
+//
+//            }
+//
+//            // Z+
+//            case 3 -> {
+//
+//            }
+//        }
+//
+//        return original.call(block, world, x, y, z);
+//    }
+//
+//    @WrapOperation(method = "getSpread", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FlowingLiquidBlock;isLiquidBreaking(Lnet/minecraft/world/World;III)Z", ordinal = 1))
+//    public boolean modifySpreadForMicroblocks(FlowingLiquidBlock block, World world, int x, int y, int z, Operation<Boolean> original) {
+//        // Y-
+//
+//        return original.call(block, world, x, y, z);
+//    }
 }
---
+++ src/main/java/net/danygames2014/microblocks/mixin/LiquidBlockMixin.java
@@ -0,0 +1,158 @@
+package net.danygames2014.microblocks.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.danygames2014.nyalib.multipart.MultipartState;
+import net.minecraft.block.LiquidBlock;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LiquidBlock.class)
+public class LiquidBlockMixin {
+    @WrapOperation(method = "getFlow", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/LiquidBlock;getLiquidDepth(Lnet/minecraft/world/BlockView;III)I", ordinal = 1))
+    public int aVoid(LiquidBlock instance, BlockView blockView, int targetX, int targetY, int targetZ, Operation<Integer> original, @Local(ordinal = 4) int var7, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+        if (blockView instanceof World world) {
+            switch (var7) {
+                // X-
+                case 1 -> {
+                    MultipartState state = world.getMultipartState(x,y,z);
+                    MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+
+                    if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z, x + 0.0625D, y + 1, z + 1))) {
+                        return 0;
+                    }
+
+                    if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX + 0.9375D, targetY, targetZ, targetX + 1, targetY + 1, targetZ + 1))) {
+                        return 0;
+                    }
+                }   
+                
+                // Z-
+                case 2 -> {
+                    MultipartState state = world.getMultipartState(x,y,z);
+                    MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+
+                    if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z, x + 1, y + 1, z + 0.0625D))) {
+                        return 0;
+                    }
+
+                    if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ + 0.9375D, targetX + 1, targetY + 1, targetZ + 1))) {
+                        return 0;
+                    }
+
+                }
+                
+                // X+
+                case 3 -> {
+                    MultipartState state = world.getMultipartState(x,y,z);
+                    MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+
+                    if (state != null && state.isBoxFullyCoevered(Box.create(x + 0.9375D, y, z, x + 1, y + 1, z + 1))) {
+                        return 0;
+                    }
+
+                    if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ, targetX + 0.0625D, targetY + 1, targetZ + 1))) {
+                        return 0;
+                    }
+
+                }
+                
+                // Z+
+                case 4 -> {
+                    MultipartState state = world.getMultipartState(x,y,z);
+                    MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+
+                    if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z  + 0.9375D, x + 1, y + 1, z + 1))) {
+                        return 0;
+                    }
+
+                    if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ, targetX + 1, targetY + 1, targetZ + 0.0625D))) {
+                        return 0;
+                    }
+                }
+            }
+        }
+        
+        return original.call(instance, blockView, targetX, targetY, targetZ);
+    }
+    
+//    // Z-
+//    @WrapOperation(method = "getFlow", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/LiquidBlock;isSolidFace(Lnet/minecraft/world/BlockView;IIII)Z", ordinal = 0))
+//    public boolean getNegativeZFlow(LiquidBlock block, BlockView blockView, int targetX, int targetY, int targetZ, int face, Operation<Boolean> original, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+//        if (blockView instanceof World world) {
+//            MultipartState state = world.getMultipartState(x,y,z);
+//            MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+//
+//            if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z, x + 1, y + 1, z + 0.0625D))) {
+//                return false;
+//            }
+//
+//            if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ + 0.9375D, targetX + 1, targetY + 1, targetZ + 1))) {
+//                return false;
+//            }
+//        }
+//        
+//        return original.call(block, blockView, targetX, targetY, targetZ, face);
+//    }
+//
+//    // Z+
+//    @WrapOperation(method = "getFlow", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/LiquidBlock;isSolidFace(Lnet/minecraft/world/BlockView;IIII)Z", ordinal = 1))
+//    public boolean getPositiveZFlow(LiquidBlock block, BlockView blockView, int targetX, int targetY, int targetZ, int face, Operation<Boolean> original, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+//        if (blockView instanceof World world) {
+//            MultipartState state = world.getMultipartState(x,y,z);
+//            MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+//
+//            if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z  + 0.9375D, x + 1, y + 1, z + 1))) {
+//                return false;
+//            }
+//
+//            if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ, targetX + 1, targetY + 1, targetZ + 0.0625D))) {
+//                return false;
+//            }
+//        }
+//        
+//        return original.call(block, blockView, targetX, targetY, targetZ, face);
+//    }
+//
+//    // X-
+//    @WrapOperation(method = "getFlow", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/LiquidBlock;isSolidFace(Lnet/minecraft/world/BlockView;IIII)Z", ordinal = 2))
+//    public boolean getNegativeXFlow(LiquidBlock block, BlockView blockView, int targetX, int targetY, int targetZ, int face, Operation<Boolean> original, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+//        if (blockView instanceof World world) {
+//            MultipartState state = world.getMultipartState(x,y,z);
+//            MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+//
+//            if (state != null && state.isBoxFullyCoevered(Box.create(x, y, z, x + 0.0625D, y + 1, z + 1))) {
+//                return false;
+//            }
+//
+//            if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX + 0.9375D, targetY, targetZ, targetX + 1, targetY + 1, targetZ + 1))) {
+//                return false;
+//            }
+//        }
+//        
+//        return original.call(block, blockView, targetX, targetY, targetZ, face);
+//    }
+//
+//    // X+
+//    @WrapOperation(method = "getFlow", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/LiquidBlock;isSolidFace(Lnet/minecraft/world/BlockView;IIII)Z", ordinal = 3))
+//    public boolean getPositiveXFlow(LiquidBlock block, BlockView blockView, int targetX, int targetY, int targetZ, int face, Operation<Boolean> original, @Local(ordinal = 0, argsOnly = true) int x, @Local(ordinal = 1, argsOnly = true) int y, @Local(ordinal = 2, argsOnly = true) int z) {
+//        if (blockView instanceof World world) {
+//            MultipartState state = world.getMultipartState(x,y,z);
+//            MultipartState targetState = world.getMultipartState(targetX,targetY,targetZ);
+//
+//            if (state != null && state.isBoxFullyCoevered(Box.create(x + 0.9375D, y, z, x + 1, y + 1, z + 1))) {
+//                return false;
+//            }
+//
+//            if (targetState != null && targetState.isBoxFullyCoevered(Box.create(targetX, targetY, targetZ, targetX + 0.0625D, targetY + 1, targetZ + 1))) {
+//                return false;
+//            }
+//        }
+//        
+//        return original.call(block, blockView, targetX, targetY, targetZ, face);
+//    }
+}
