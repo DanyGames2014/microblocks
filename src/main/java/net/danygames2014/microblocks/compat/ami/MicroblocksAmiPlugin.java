c22bb306cf1484069b26d9b76a6eb9f7430463c5
Daniel Fildán <fildand@centrum.cz>
2026-02-15 21:51:12 +0100

    AMI Plugin

---
+++ src/main/java/net/danygames2014/microblocks/compat/ami/MicroblocksAmiPlugin.java
@@ -0,0 +1,66 @@
+package net.danygames2014.microblocks.compat.ami;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.danygames2014.microblocks.Microblocks;
+import net.danygames2014.microblocks.item.base.MicroblockItem;
+import net.glasslauncher.mods.alwaysmoreitems.api.*;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.modificationstation.stationapi.api.util.Identifier;
+
+public class MicroblocksAmiPlugin implements ModPluginProvider {
+    public MicroblocksAmiPlugin() {
+        super();
+    }
+
+    @Override
+    public String getName() {
+        return "Microblocks";
+    }
+
+    @Override
+    public Identifier getId() {
+        return Microblocks.NAMESPACE.id("microblocks");
+    }
+
+    @Override
+    public void onAMIHelpersAvailable(AMIHelpers amiHelpers) {
+        
+    }
+
+    @Override
+    public void onItemRegistryAvailable(ItemRegistry itemRegistry) {
+
+    }
+
+    @Override
+    public void register(ModRegistry registry) {
+
+    }
+
+    @Override
+    public void onRecipeRegistryAvailable(RecipeRegistry recipeRegistry) {
+
+    }
+
+    @Override
+    public SyncableRecipe deserializeRecipe(NbtCompound recipe) {
+        return null;
+    }
+
+    @Override
+    public void updateBlacklist(AMIHelpers amiHelpers) {
+        for (var microblockItemsOfType : Microblocks.microblockItems.values()) {
+            for (var microblockItems : microblockItemsOfType.values()) {
+                for (Int2ObjectMap.Entry<MicroblockItem> microblockItem : microblockItems.int2ObjectEntrySet()) {
+                    if (microblockItem.getValue().block == Block.STONE) {
+                        continue;
+                    }
+                    
+                    amiHelpers.getItemBlacklist().addItemToBlacklist(new ItemStack(microblockItem.getValue(), 1, microblockItem.getIntKey()));
+                }
+            }
+        }
+    }
+}
